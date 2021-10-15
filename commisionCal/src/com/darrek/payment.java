42cd43a667f7b3f79ef86f6afeb418fc420bd18e
Darrek Lau <Lau.kok.tiong@gmail.com>
2021-10-15 15:13:36 +0800

    Change line 21, 24 and 27 for commissionCal payment.java

--- commisionCal/src/com/darrek/payment.java
+++ commisionCal/src/com/darrek/payment.java
@@ -18,13 +18,13 @@ public class payment {
     public static void check() {
         if (salesAmount > 10000) {
             commission = salesAmount * 0.30;
-            System.out.printf("Commission is: " + commission);
+            System.out.println("Commission is: " + commission);
         } else if (salesAmount > 5000) {
             commission = salesAmount * 0.20;
-            System.out.printf("Commission is: " + commission);
+            System.out.println("Commission is: " + commission);
         } else if (salesAmount > 1000) {
             commission = salesAmount * 0.10;
-            System.out.printf("Commission is: " + commission);
+            System.out.println("Commission is: " + commission);
         } else
             System.out.println("Commission: N/A");
     }
